9cce3b0b192dc87b5bc49a4f579df755c2515a30
Eugene Shapovalov <eugene@evernetica.com>
2021-10-19 19:12:42 +0300

    add map

--- src/pages/MapPage/MapPage.js
+++ src/pages/MapPage/MapPage.js
@@ -1,11 +1,57 @@
-import React from 'react'
-import StyledView from "../../commonComponents/lowLevelComponents/styledView";
+import React, {useState, useEffect} from 'react'
+import { Dimensions } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import { Platform, PermissionsAndroid } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+import {PERMISSIONS, request} from "react-native-permissions";
 
 const MapPage = () => {
-  return(
-    <StyledView>
+  const [windowSize] = useState(Dimensions.get('window'))
+  const [currentPosition, setCurrentPosition] = useState(null)
+  const [region, setRegion] = useState({
+    latitude: 47.8388,
+    longitude: 35.139567,
+    latitudeDelta: 0.1922,
+    longitudeDelta: 0.0421,
+  })
 
-    </StyledView>
+  useEffect(async () => {
+    if (Platform.OS === 'ios') {
+      await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE)
+
+    } else {
+      await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      );
+    }
+    Geolocation.getCurrentPosition(
+      (position) => {
+        setCurrentPosition({latitude: position.coords.latitude, longitude: position.coords.longitude})
+      },
+      (error) => {
+        // See error code charts below.
+        console.log(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    );
+  }, [])
+
+  return (
+    <MapView
+      width={windowSize.width}
+      height={windowSize.height}
+      region={region}
+    >
+      <Marker
+        coordinate={{
+          latitude: 47.8388,
+          longitude: 35.139567,
+        }}
+      />
+      {currentPosition && <Marker
+        coordinate={currentPosition}
+      />}
+    </MapView>
   )
 }
 
