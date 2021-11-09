409e355d232ad2285be835d4dec00395367a64aa
AlexeyBazilevskiy <AlexeyBazilevsky@evernetica.com>
2021-11-09 16:00:55 +0200

    need in celebrating

--- src/pages/CameraPage/CameraPage.js
+++ src/pages/CameraPage/CameraPage.js
@@ -1,12 +1,17 @@
 import React, {useState, useEffect} from 'react'
 import { RNCamera } from 'react-native-camera';
 import CameraRoll from '@react-native-community/cameraroll'
+import { accelerometer, setUpdateIntervalForType, SensorTypes } from "react-native-sensors";
 import {Dimensions, Platform, SafeAreaView} from 'react-native'
 import CustomButton from "../../commonComponents/complexedComponents/CustomButton/CustomButton";
 import Gallery from "../../commonComponents/complexedComponents/Gallery/Gallery";
 import {useDispatch, useSelector} from 'react-redux'
 import {savePictures, saveLocalPictures} from "../../actions/actionCreator";
 import NotifService from '../../notification/NotifService';
+import StyledView from "../../commonComponents/lowLevelComponents/styledView";
+import StyledText from "../../commonComponents/lowLevelComponents/styledText";
+
+setUpdateIntervalForType(SensorTypes.accelerometer, 200); // defaults to 100ms
 
 const CameraPage = () => {
   const [windowSize] = useState(Dimensions.get('window'))
@@ -14,6 +19,7 @@ const CameraPage = () => {
   const dispatch = useDispatch()
   const {photoStore: {data}} = useSelector(store => store)
   const [notif] = useState(new NotifService())
+  const [x, setX] = useState(0)
 
   useEffect(async () => {
     if(data.length === 0) {
@@ -22,6 +28,19 @@ const CameraPage = () => {
       data.edges.filter(item => uris.push(item.node.image.uri))
       dispatch(saveLocalPictures(uris))
     }
+    const subscription = accelerometer.subscribe(
+      ({x}) => {
+        // const val = Platform.OS === "ios" ? Math.floor(x * (-90)) : Math.floor(x * 9)
+        const val = Math.atan2(y_Buff , z_Buff) * 57.3;
+        // const val = atan2((- x_Buff) , sqrt(y_Buff * y_Buff + z_Buff * z_Buff)) * 57.3;
+        console.log(val);
+        setX(val)
+      },
+      (err) => {
+        console.log(err)
+      },
+    );
+    return (() => subscription.unsubscribe())
   }, [])
 
   const onSnap = async () => {
@@ -56,6 +75,24 @@ const CameraPage = () => {
         }}
         onPress={onSnap}
       />
+      <StyledView
+        width={'50%'}
+        height={'5px'}
+        backgroundColor={'white'}
+        alignSelf={'center'}
+        style={{position: 'absolute', top: windowSize.height * .5, transform: [{ rotate: `${x}deg` }]}}
+      />
+      <StyledText
+        style={{
+          position: 'absolute',
+          top: windowSize.height * .52,
+          zIndex: 3
+        }}
+        color={'black'}
+        fontWeight={'bold'}
+        alignSelf={'center'}
+      >{Math.floor(x)} deg
+      </StyledText>
       <Gallery
         contentContainerStyle={{flexDirection: 'row'}}
         styles={{position: 'absolute', top: Platform.OS === 'ios' ? windowSize.height * .75 : windowSize.height * .75}}
